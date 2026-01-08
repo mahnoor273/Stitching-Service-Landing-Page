8020f81ab3150f453fa5a7c09a1c2d44f85bff0e
Mahnoor <mahnoor231hussain@gmail.com>
2026-01-08 02:08:56 -0800

    Working on project....

--- src/Pages/Chooseyourfabric.jsx
+++ src/Pages/Chooseyourfabric.jsx
@@ -10,6 +10,17 @@ import card6 from "../assets/cc5.png";
 import card7 from "../assets/cc6.png";
 
 const ChooseYourFabric = () => {
+  const fabrics = [
+    { img: card, name: "Lilan Cloth", price: "1000RS", link: "/liloncloth" },
+    { img: card1, name: "Cotton Cloth", price: "1200RS", link: "/Cottoncloth" },
+    { img: card2, name: "Silk Cloth", price: "1500RS", link: "/silkcloth" },
+    { img: card3, name: "Wool Cloth", price: "2500RS", link: "/woolcloth" },
+    { img: card4, name: "Organza Cloth", price: "1100RS", link: "/organzacloth" },
+    { img: card5, name: "Velvet Cloth", price: "3000RS", link: "/velvetcloth" },
+    { img: card6, name: "Satin Cloth", price: "1800RS", link: "/satincloth" },
+    { img: card7, name: "Rayon Cloth", price: "1500RS", link: "/rayoncloth" },
+  ];
+
   return (
     <section
       style={{
@@ -23,6 +34,7 @@ const ChooseYourFabric = () => {
           fontWeight: "bold",
           fontSize: "70px",
           marginTop: "120px",
+          textAlign: "center", // 👈 Heading center
         }}
       >
         Choose Your Fabric
@@ -32,21 +44,13 @@ const ChooseYourFabric = () => {
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "repeat(4, minmax(260px, 1fr))", // 👈 FIX
+          gridTemplateColumns: "repeat(4, minmax(260px, 1fr))",
           gap: "30px",
           marginTop: "60px",
+          justifyContent: "center",
         }}
       >
-        {[
-          { img: card, name: "Lilan Cloth", price: "1000RS", link: "/liloncloth" },
-          { img: card1, name: "Cotton Cloth", price: "1200RS", link: "/Cottoncloth" },
-          { img: card2, name: "Silk Cloth", price: "1500RS", link: "/silkcloth" },
-          { img: card3, name: "Wool Cloth", price: "2500RS", link: "/woolcloth" },
-          { img: card4, name: "Organza Cloth", price: "1100RS", link: "/organzacloth" },
-          { img: card5, name: "Velvet Cloth", price: "3000RS", link: "/velvetcloth" },
-          { img: card6, name: "Satin Cloth", price: "1800RS", link: "/satincloth" },
-          { img: card7, name: "Rayon Cloth", price: "1500RS", link: "/rayoncloth" },
-        ].map((item, i) => (
+        {fabrics.map((item, i) => (
           <Link
             key={i}
             to={item.link}
@@ -55,11 +59,12 @@ const ChooseYourFabric = () => {
             <div
               className="card"
               style={{
-                maxWidth: "280px",        // 👈 CARD CHOTI
+                maxWidth: "280px",
                 height: "470px",
-                margin: "0 auto",        // center inside grid
+                margin: "0 auto",
                 display: "flex",
                 flexDirection: "column",
+                justifyContent: "space-between",
               }}
             >
               <img
@@ -68,6 +73,7 @@ const ChooseYourFabric = () => {
                 style={{
                   height: "230px",
                   objectFit: "cover",
+                  borderRadius: "10px",
                 }}
               />
 
@@ -77,29 +83,38 @@ const ChooseYourFabric = () => {
                   flex: 1,
                   display: "flex",
                   flexDirection: "column",
+                  alignItems: "center", // 👈 Button center
                 }}
               >
-                <h5 style={{ fontSize: "32px", fontWeight: "bold" }}>
+                <h5 style={{ fontSize: "32px", fontWeight: "bold", textAlign: "center" }}>
                   {item.name}
                 </h5>
 
                 <p style={{ fontSize: "16px" }}>Price</p>
                 <h6 style={{ fontSize: "20px" }}>{item.price}</h6>
 
-                <div style={{ marginTop: "auto", textAlign: "right" }}>
+                <div style={{ marginTop: "auto", textAlign: "center" }}>
                   <button
-  style={{
-    backgroundColor: "#7303a0ff",
-    padding: "10px 28px",
-    fontSize: "16px",
-    border: "none",
-    borderRadius: "6px",
-    fontWeight: "bold",
-    cursor: "pointer",
-    minWidth: "140px",
-  }}
->
-
+                    style={{
+                      backgroundColor: "#7303a0ff",
+                      padding: "10px 28px",
+                      fontSize: "16px",
+                      border: "none",
+                      borderRadius: "6px",
+                      fontWeight: "bold",
+                      cursor: "pointer",
+                      minWidth: "140px",
+                      transition: "all 0.3s ease",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.target.style.backgroundColor = "#b800bd";
+                      e.target.style.color = "white";
+                    }}
+                    onMouseLeave={(e) => {
+                      e.target.style.backgroundColor = "#7303a0ff";
+                      e.target.style.color = "black";
+                    }}
+                  >
                     Buy Now
                   </button>
                 </div>
@@ -113,4 +128,5 @@ const ChooseYourFabric = () => {
 };
 
 export default ChooseYourFabric;
+
 
